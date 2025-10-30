f804d35146ed1b0bab05ef5211504fc464d2e41b
benj652 <benjaminjaffe8@gmail.com>
2025-10-30 16:27:42 -0400

    made a base login page at /user/login

--- views/user_views.py
+++ views/user_views.py
@@ -1,3 +1,13 @@
-'''
+"""
 Auth views
-'''
+"""
+
+from flask import Blueprint, render_template
+from constants import LOGIN_ROUTE, LOGIN_TEMPLATE, USER_BLUEPRINT_NAME
+
+user_blueprint = Blueprint(USER_BLUEPRINT_NAME, __name__)
+
+
+@user_blueprint.route(LOGIN_ROUTE)
+def login():
+    return render_template(LOGIN_TEMPLATE)
