2d711726a52df6291e07fdaec8e9eb7dfbd9bb0f
voteme4 <zachberg2004@gmail.com>
2025-11-01 17:37:19 -0400

    Simple mock for main page

--- views/home_views.py
+++ views/home_views.py
@@ -5,7 +5,7 @@ from flask_login import login_required, current_user
 home_blueprint = Blueprint('home', __name__)
 
 @home_blueprint.route('/')
-@login_required
+#@login_required
 def home():
     print(f"Current user: {current_user}")
     return render_template('home.html')
